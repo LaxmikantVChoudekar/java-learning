7ee9700d8a3f596df8a216199b29ffc9812720fc
LaxmikantVChoudekar <lvchoudekar@gmail.com>
2026-06-19 08:37:09 +0530

    Implement electricity bill calculation logic
    electricity bill calculate by following parameter
    up to 100 unit =  5 Rupees/Unit
    next 100  unit  = 7  Rupees/Unit
    above 200 unit = 10 Rupees/Unit.

--- decision-making/ElectricityBillCla.java
+++ decision-making/ElectricityBillCla.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+
+public class ElectricityBillCla {
+    public static void main(String[] args) {
+    int unit, bill=0;
+    System.out.print("enter electricity unit ");
+    Scanner ip = new Scanner(System.in);
+    unit = ip.nextInt();
+    ip.close();
+      if (unit < 0 ) {
+        System.out.println("electricity unit can not be negative " );
+      }
+     //up to 100 unit rate 5 rupee   
+    else if(unit <=100){
+        bill = unit * 5;
+    }
+      //next  100 unit rate 7 rupee   
+    else if (unit > 100 && unit<=200)
+    {
+        bill = (100 * 5) + ( unit -100 ) *7;
+    }
+        // next unit rate 10 ruoee
+    else {
+        bill = (100 * 5) + ( 100 * 7) + (unit -200) *10;
+    }
+     System.out.println("Electricity Bill: ₹" + bill);
+  }
+} 
