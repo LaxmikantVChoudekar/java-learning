9488c849403c10872ab2cded7504505bb7a237e6
LaxmikantVChoudekar <lvchoudekar>
2026-06-19 20:42:43 +0530

    electrity can not negative

--- decision-making/ElectricityBillCla.java
+++ decision-making/ElectricityBillCla.java
@@ -11,19 +11,22 @@ public class ElectricityBillCla {
       if (unit < 0 ) {
         System.out.println("electricity unit can not be negative " );
       }
-     //up to 100 unit rate 5 rupee   
-    else if(unit <=100){
-        bill = unit * 5;
-    }
-      //next  100 unit rate 7 rupee   
-    else if (unit > 100 && unit<=200)
-    {
-        bill = (100 * 5) + ( unit -100 ) *7;
-    }
-        // next unit rate 10 ruoee
-    else {
-        bill = (100 * 5) + ( 100 * 7) + (unit -200) *10;
-    }
-     System.out.println("Electricity Bill: ₹" + bill);
+      else{
+             //up to 100 unit rate 5 rupee
+             if(unit <=100){
+              bill = unit * 5;
+             }
+            //next  100 unit rate 7 rupee
+             else if (unit > 100 && unit<=200)
+            {
+                  bill = (100 * 5) + ( unit -100 ) *7;
+            }
+            // next unit rate 10 ruoee
+            else {
+                  bill = (100 * 5) + ( 100 * 7) + (unit -200) *10;
+            }
+            System.out.println("Electricity Bill: ₹" + bill);
+      }
+
   }
 } 
