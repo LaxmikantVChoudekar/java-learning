0fa86a677ac2a8ec3d415365ca3e14dc1ac7bba9
LaxmikantVChoudekar <lvchoudekar>
2026-06-21 08:38:59 +0530

    check Number

--- decision-making/checkNumber.java
+++ decision-making/checkNumber.java
@@ -1,3 +1,22 @@
-public class checkNumber {
+import java.util.Scanner;
 
+public class checkNumber {
+    public static void main (String []arg){
+        int number;
+        System.out.print("enter any number = ");
+        Scanner ip = new Scanner(System.in);
+        number = ip.nextInt();
+
+        if(number < 0)
+        {
+            System.out.print("given number are negative .");
+        }
+        else if(number > 0)
+        {
+            System.out.print("given number are positive .");
+        }
+        else {
+            System.out.print("given number are zero .");
+        }
+    }
 }
