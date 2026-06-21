80529fbe138ae63134778ef1a9b4e1d33c36c199
LaxmikantVChoudekar <lvchoudekar>
2026-06-21 09:27:11 +0530

    logic are bulid

--- decision-making/checkLeapYear.java
+++ decision-making/checkLeapYear.java
@@ -3,6 +3,30 @@ import java.util.Scanner;
 public class checkLeapYear {
     public static void main (String [] arg )
     {
+        int year;
+        boolean isLeapYear = false;
+        System.out.print("enter year = ");
+        Scanner ip = new Scanner(System.in);
+        year = ip.nextInt();
+        if(year%4==0)
+        {
+            isLeapYear = true;
+            if(year%100==0)
+            {
+                isLeapYear = false;
+                if(year%400==0)
+                {
+                    isLeapYear = true;
+                }
+            }
+        }
 
+      if (isLeapYear)
+      {
+          System.out.print("Yes , "+ year + " is leap year. ");
+      }
+      else {
+          System.out.print("No , "+ year + " is not leap year. ");
+      }
     }
 }
