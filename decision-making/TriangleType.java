d618c7c5371ad12515765ee3b67896b1a263c88b
LaxmikantVChoudekar <lvchoudekar>
2026-06-22 00:43:35 +0530

    invalid input can show

--- decision-making/TriangleType.java
+++ decision-making/TriangleType.java
@@ -4,21 +4,26 @@ public class TriangleType {
     public static void main(String []arg )
     {
         int side1 ,side2 ,side3;
-        System.out.print("enter length of 3 sides  of triengle ");
+        System.out.println("enter length of 3 sides  of triengle ");
         Scanner ip = new Scanner(System.in);
         side1 = ip.nextInt();
         side2 = ip.nextInt();
         side3 = ip.nextInt();
-        if  (side1 == side2 && side1 == side3)
+        ip.close();
+        if(!(side1 + side2 > side3 && side2 + side3 > side1 && side3 + side1 > side2 ))
         {
-            System.out.print("on given side length the thriengle is Equlaterl ");
+            System.out.println("invalid input .");
+        }
+        else if  (side1 == side2 && side1 == side3)
+        {
+            System.out.println("on given side length the thriengle is Equilateral ");
         }
         else if (side1 == side2 || side1 ==side3 || side2 == side3 )
        {
-            System.out.print("on given side length the thriengle is Isosceles ");
+            System.out.println("on given side length the thriengle is Isosceles ");
        }
         else {
-            System.out.print("on given side length the thriengle is Scalene ");
+            System.out.println("on given side length the thriengle is Scalene ");
         }
     }
 }
