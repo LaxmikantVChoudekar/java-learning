514a8df5af95c3611649b31e4b9dc446df269dda
LaxmikantVChoudekar <lvchoudekar>
2026-06-23 08:55:22 +0530

    use of switch

---
+++ decision-making/TraficLightSystem.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TraficLightSystem {
+    public static void main(String[]arg ) {
+        String traficColor ;
+        System.out.print("enter trafic light collor ");
+        Scanner ip =new Scanner(System.in);
+        traficColor = ip.next();
+        ip.close();
+        switch (traficColor){
+            case  "Red":
+                System.out.println("Stop");
+                break;
+            case "Yellow" :
+                System.out.println("Prepare to stop");
+                break;
+            case "Green" :
+                System.out.println("Go");
+                break;
+            default:
+                System.out.println("Ivalid input ");
+        }
+    }
+}
